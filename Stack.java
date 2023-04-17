8f222431964c6c28dfada2d04a1ce5a72f488273
Kerim <kerim.sabic.207@gmail.com>
2023-04-17 21:46:07 +0200

    Stack

--- Stack.java
+++ Stack.java
@@ -1,29 +1,41 @@
-
 public class Stack<Item> {
-	private Node<Item> top = null;
+
+    /* Node implementation */
+    private class Node <Item>{
+        Item data;
+        Node<Item> next;
+    }
+
+    private Node<Item> top = null;
     private int size = 0;
- 
-    
+
     /* Push an item onto the stack */
     public void push(Item item) {
-    	// your code
+        Node<Item>newNode=new Node<Item>();
+        newNode.data=item;
+        newNode.next=top;
+        top=newNode;
+        size++;
     }
-    
+
     /* Check if the stack is empty */
     public boolean isEmpty() {
-    	// your code
-		return false;
+        return top==null;
     }
-    
+
     /* Remove the top item from the stack, and return its data */
     public Item pop() {
-    	// your code
-        return null;
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException();
+        }
+        Item dataToRead=this.top.data;
+        this.top=this.top.next;
+        this.size--;
+        return dataToRead;
     }
-    
+
     /* Return the current size of the stack */
     public int size() {
-    	// your code
-        return 0;
+        return this.size;
     }
 }
